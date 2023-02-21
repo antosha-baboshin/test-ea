d6e3f48b7c937d939c489ab9b9b670e87dbcfc7f
Antosha Baboshin <antosha.js@gmail.com>
2023-02-22 00:37:47 +0300

    feat: add pageSwitcher

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import startTimer from './components/timer.js';
 import validateForm from './components/validation.js';
+import switchPage from './components/pageSwither.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   startTimer();
   validateForm();
+  switchPage();
 });
