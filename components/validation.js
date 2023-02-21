284ce387cc0e2f3fe82c5d12e3e5d5c14af95ad1
Antosha Baboshin <antosha.js@gmail.com>
2023-02-21 20:05:08 +0300

    feat: add resopnse

--- components/validation.js
+++ components/validation.js
@@ -1,22 +1,41 @@
 const validateForm = () => {
   const FORM = document.querySelector('.footer__frame');
   const FORM_INPUT = document.querySelector('.footer__input');
+  const URL = 'http://localhost:3000/notification';
 
   const checkEmail = () => {
     const EMAIL = FORM_INPUT.value;
-    return /^[\w]{1}[\w-.]*@[\w-]+\.[a-z]{2,4}$/i.test(EMAIL);
+    return !/^[\w]{1}[\w-.]*@[\w-]+\.[a-z]{2,4}$/i.test(EMAIL);
+  };
+
+  const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data),
+    });
+    return response.json();
   };
 
   const sendForm = async () => {
     const ERROR = checkEmail();
-    // const EMAIL = FORM_INPUT.value;
+    const EMAIL = FORM_INPUT.value;
 
-    if (!ERROR) {
+    if (ERROR) {
       FORM.classList.add('_invalid');
       FORM_INPUT.classList.add('_invalid');
     } else {
       FORM.classList.remove('_invalid');
       FORM_INPUT.classList.remove('_invalid');
+
+      postData(URL, { email: EMAIL }).then((data) => console.log(data)).catch('Error');
     }
   };
 
